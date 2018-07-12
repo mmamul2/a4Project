6ff51c2974234209ed5911c478401a5d571a596d
mmacmul2 <39165116+mmacmul2@users.noreply.github.com>
2018-07-12 13:28:20 -0300

    Finished refactoring step 2 and cleaned up indentation

--- src/RegularPendulum.java
+++ src/RegularPendulum.java
@@ -11,24 +11,25 @@ public class RegularPendulum extends AbstractPendulum {
      */
     public RegularPendulum (double inLength, double inMass, double inTheta0, 
 		     double inDelta, double inDiss, GravityModel g) {
-	super (inLength, inMass, inTheta0, g);
-	delta=inDelta;
-	dissipation = inDiss;
-	lastVel = 0;
-	lastTheta = this.getMaxAngularDisplacement ();
-	lastAccel = -(this.getGravitationalField () / this.getStringLength ())*Math.sin (lastTheta);
+	    super (inLength, inMass, inTheta0, g);
+	    delta=inDelta;
+	    dissipation = inDiss;
+	    lastVel = 0;
+	    lastTheta = this.getMaxAngularDisplacement ();
+	    lastAccel = -(this.getGravitationalField () / this.getStringLength ())*Math.sin (lastTheta);
     }
 
     public RegularPendulum (double inLength, double inMass, double inTheta0, 
 		     double inDelta, GravityModel g) {
-	this (inLength, inMass, inTheta0, inDelta, 0, g);
+	    this (inLength, inMass, inTheta0, inDelta, 0, g);
     }
 
     public void step () {
-	iterations++;
-	lastTheta = lastTheta + lastVel*delta;
-	lastVel = lastVel + lastAccel*delta;
-	lastAccel = - dissipation*lastVel - this.getGravitationalField () / this.getStringLength () * Math.sin (lastTheta);
+	    iterations++;
+	    lastTheta = lastTheta + lastVel*delta;
+	    lastVel = lastVel + lastAccel*delta;
+	    lastAccel = - dissipation*lastVel - this.getGravitationalField () / this.getStringLength ()
+                * Math.sin (lastTheta);
     }
 
     public double getLastTheta () { return lastTheta; }
@@ -36,6 +37,4 @@ public class RegularPendulum extends AbstractPendulum {
     public double getLastAcceleration () { return lastAccel; }
     public double getLastTime () { return iterations*delta; }
     public double getDissipationConstant () { return dissipation; }
-    
-
 }
